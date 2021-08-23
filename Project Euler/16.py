a19d763bd7001854e83a27920ecbc8e3b30fc3d8
Deepesh Nimma <deepnimma@gmail.com>
2021-08-23 17:06:01 +0530

    16 and 17 project euler

---
+++ Project Euler/16.py
@@ -0,0 +1,13 @@
+import time
+def sum_of_digits(num: int):
+    sum = 0
+    num_string = str(num)
+    list_num = list(num_string)
+
+    for i in list_num:
+        sum += int(i)
+
+    return sum
+
+# print(2 ** 1000)
+print(sum_of_digits(2 ** 1000))
